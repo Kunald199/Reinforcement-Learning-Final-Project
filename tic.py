10781d60c62df33516ae5666a9ffa458d1d2f05d
Kunal Ajay Dhavale <65370361+Kunald199@users.noreply.github.com>
2021-04-17 17:26:06 -0400

    Add files via upload

---
+++ tic.py
@@ -0,0 +1,365 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from epsilonAgent import epsilonAgent
+
+class State:
+    def __init__(self, p1, p2):
+        self.board = np.zeros((3, 3))
+        self.p1 = p1
+        self.p2 = p2
+        self.isEnd = False
+        self.boardHash = None
+        # init p1 plays first
+        self.playerSymbol = 1
+
+    # get unique hash of current board state
+    def getHash(self):
+        self.boardHash = str(self.board.reshape(3 * 3))
+        return self.boardHash
+
+    def winner(self):
+        # row
+        for i in range(3):
+            if sum(self.board[i, :]) == 3:
+                self.isEnd = True
+                return 1
+            if sum(self.board[i, :]) == -3:
+                self.isEnd = True
+                return -1
+        # col
+        for i in range(3):
+            if sum(self.board[:, i]) == 3:
+                self.isEnd = True
+                return 1
+            if sum(self.board[:, i]) == -3:
+                self.isEnd = True
+                return -1
+        # diagonal
+        diag_sum1 = sum([self.board[i, i] for i in range(3)])
+        diag_sum2 = sum([self.board[i, 3 - i - 1] for i in range(3)])
+        diag_sum = max(abs(diag_sum1), abs(diag_sum2))
+        if diag_sum == 3:
+            self.isEnd = True
+            if diag_sum1 == 3 or diag_sum2 == 3:
+                return 1
+            else:
+                return -1
+
+        # tie
+        # no available positions
+        if len(self.availablePositions()) == 0:
+            self.isEnd = True
+            return 0
+        # not end
+        self.isEnd = False
+        return None
+
+    def availablePositions(self):
+        positions = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i, j] == 0:
+                    positions.append((i, j))  # need to be tuple
+        return positions
+
+    def updateState(self, position):
+        self.board[position] = self.playerSymbol
+        # switch to another player
+        self.playerSymbol = -1 if self.playerSymbol == 1 else 1
+
+    # only when game ends
+    def giveReward(self):
+        result = self.winner()
+        # backpropagate reward
+        if result == 1:
+            self.p1.feedReward(1)
+            self.p2.feedReward(0)
+        elif result == -1:
+            self.p1.feedReward(0)
+            self.p2.feedReward(1)
+        else:
+            self.p1.feedReward(0.1)
+            self.p2.feedReward(0.5)
+
+    # board reset
+    def reset(self):
+        self.board = np.zeros((3, 3))
+        self.boardHash = None
+        self.isEnd = False
+        self.playerSymbol = 1
+
+    def play(self, rounds=100):
+        results = []
+        for i in range(rounds):
+            if i % 1000 == 0 and i != 0:
+                print("Epochs {}".format(i))
+            while not self.isEnd:
+                # Player 1
+                positions = self.availablePositions()
+                p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+                # take action and upate board state
+                self.updateState(p1_action)
+                board_hash = self.getHash()
+                self.p1.addState(board_hash)
+                # check board status if it is end
+
+                win = self.winner()
+                if win is not None:
+                    # self.showBoard()
+                    # ended with p1 either win or draw
+                    self.giveReward()
+                    self.p1.reset()
+                    self.p2.reset()
+                    self.reset()
+                    results.append(win)
+                    break
+                else:
+                    # Player 2
+                    positions = self.availablePositions()
+                    p2_action = self.p2.chooseAction(positions, self.board, self.playerSymbol)
+                    self.updateState(p2_action)
+                    board_hash = self.getHash()
+                    self.p2.addState(board_hash)
+
+                    win = self.winner()
+                    if win is not None:
+                        # self.showBoard()
+                        # ended with p2 either win or draw
+                        self.giveReward()
+                        self.p1.reset()
+                        self.p2.reset()
+                        self.reset()
+                        results.append(win)
+                        break
+        return results
+
+    # play with human
+    def play2(self):
+        while not self.isEnd:
+            # Player 1
+            positions = self.availablePositions()
+            p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+            # take action and upate board state
+            self.updateState(p1_action)
+            self.showBoard()
+            # check board status if it is end
+            win = self.winner()
+            if win is not None:
+                if win == 1:
+                    print(self.p1.name, "wins!")
+                else:
+                    print("tie!")
+                self.reset()
+                break
+
+            else:
+                # Player 2
+                positions = self.availablePositions()
+                p2_action = self.p2.chooseAction(positions)
+
+                self.updateState(p2_action)
+                self.showBoard()
+                win = self.winner()
+                if win is not None:
+                    if win == -1:
+                        print(self.p2.name, "wins!")
+                    else:
+                        print("tie!")
+                    self.reset()
+                    break
+
+
+    def compete(self, num_games=10):
+        p1wins = [0]
+        p2wins = [0]
+        ties = [0]
+        print("Agents competing....")
+        for i in range(0, num_games):
+            print("Round: ",i)
+            while not self.isEnd:
+                positions = self.availablePositions()
+                p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
+                self.updateState(p1_action)
+
+                win = self.winner()
+                if win is not None:
+                    # append.
+                    if win == 1:
+                        p1wins.append(p1wins[-1] + 1)
+                        ties.append(ties[-1])
+                        p2wins.append(p2wins[-1])
+                    else:
+                        ties.append(ties[-1] + 1)
+                        p1wins.append(p1wins[-1])
+                        p2wins.append(p2wins[-1])
+                    self.reset()
+                    break
+                else:
+                    positions = self.availablePositions()
+                    p2_action = self.p2.chooseAction(positions, self.board, self.playerSymbol)
+                    self.updateState(p2_action)
+                    win = self.winner()
+                    if win is not None:
+                        if win == -1:
+                            p2wins.append(p2wins[-1] + 1)
+                            ties.append(ties[-1])
+                            p1wins.append(p1wins[-1])
+                        else:
+                            ties.append(ties[-1] + 1)
+                            p1wins.append(p1wins[-1])
+                            p2wins.append(p2wins[-1])
+                        self.reset()
+                        break
+
+        # do the printout here
+        count = list(range(0,num_games+1))
+        plt.ylabel('Outcomes')
+        plt.xlabel('Number of Games')
+        plt.title('P1: ({}) vs. P2: ({})'.format(self.p1.name, self.p2.name))
+
+        plt.plot(count, p1wins, 'b-', label='P1 Win')
+        plt.plot(count, p2wins, 'r-', label='P2 Win')
+        plt.plot(count, ties, 'g-', label='Tie')
+        plt.legend(loc='best')
+        plt.show()
+
+
+
+    def showBoard(self):
+        # p1: x  p2: o
+        for i in range(0, 3):
+            print('-------------')
+            out = '| '
+            for j in range(0, 3):
+                if self.board[i, j] == 1:
+                    token = 'X'
+                if self.board[i, j] == -1:
+                    token = 'O'
+                if self.board[i, j] == 0:
+                    token = ' '
+                out += token + ' | '
+            print(out)
+        print('-------------')
+
+
+class Player:
+    def __init__(self, name, exp_rate=0.3):
+        self.name = name
+        self.states = []  # record all positions taken
+        self.lr = 0.2
+        self.exp_rate = exp_rate
+        self.decay_gamma = 0.9
+        self.states_value = {}  # state -> value
+
+    def getHash(self, board):
+        boardHash = str(board.reshape(3 * 3))
+        return boardHash
+
+    def chooseAction(self, positions, current_board, symbol):
+        if np.random.uniform(0, 1) <= self.exp_rate:
+            # take random action
+            idx = np.random.choice(len(positions))
+            action = positions[idx]
+        else:
+            value_max = -999
+            for p in positions:
+                next_board = current_board.copy()
+                next_board[p] = symbol
+                next_boardHash = self.getHash(next_board)
+                value = 0 if self.states_value.get(next_boardHash) is None else self.states_value.get(next_boardHash)
+                # print("value", value)
+                if value >= value_max:
+                    value_max = value
+                    action = p
+        # print("{} takes action {}".format(self.name, action))
+        return action
+
+    # append a hash state
+    def addState(self, state):
+        self.states.append(state)
+
+    # at the end of game, backpropagate and update states value
+    def feedReward(self, reward):
+        for st in reversed(self.states):
+            if self.states_value.get(st) is None:
+                self.states_value[st] = 0
+            self.states_value[st] += self.lr * (self.decay_gamma * reward - self.states_value[st])
+            reward = self.states_value[st]
+
+    def reset(self):
+        self.states = []
+
+    def savePolicy(self):
+        fw = open('policy_' + str(self.name), 'wb')
+        pickle.dump(self.states_value, fw)
+        fw.close()
+
+    def loadPolicy(self, file):
+        fr = open(file, 'rb')
+        self.states_value = pickle.load(fr)
+        fr.close()
+
+
+class HumanPlayer:
+    def __init__(self, name):
+        self.name = name
+
+    def chooseAction(self, positions):
+        while True:
+            pos = int(input("Input your position: ")) - 1
+            row = pos // 3
+            col = pos % 3
+            action = (row, col)
+            if action in positions:
+                return action
+
+    # append a hash state
+    def addState(self, state):
+        pass
+
+    # at the end of game, backpropagate and update states value
+    def feedReward(self, reward):
+        pass
+
+    def reset(self):
+        pass
+
+
+def train(num_epochs=5000):
+    p1 = Player("p1")
+    p2 = Player("p2")
+
+    st = State(p1, p2)
+    print("training...")
+    st.play(num_epochs)
+    p1.savePolicy()
+    p2.savePolicy()
+
+def compete_q_vs_q(num_games=100):
+    p1 = Player("p1",exp_rate=0)
+    p2 = Player("p2",exp_rate=0)
+    p1.loadPolicy("policy_p1")
+    p2.loadPolicy("policy_p2")
+    st = State(p1, p2)
+    st.compete(num_games)
+
+def compete_q_vs_rand(num_games=100):
+    p1 = Player("Q-Learning",exp_rate=0)
+    p2 = epsilonAgent("Epsilon Agent")
+    p1.loadPolicy("policy_p1")
+    st = State(p1, p2)
+    st.compete(num_games)
+
+def play():
+    while True:
+        p1 = Player("p1",exp_rate=0)
+        p1.loadPolicy("policy_p1")
+        p2 = HumanPlayer("human")
+        st = State(p1, p2)
+        st.play2()
+
+train(num_epochs=5000)
+compete_q_vs_q(num_games=1000)
+# compete_q_vs_rand(num_games=10000)
+#play()
